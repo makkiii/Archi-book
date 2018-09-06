f72ec85ce46f228623126d0d3b21f4c8fa771f5c
shinomaki <makki@shinoharamakikonoMacBook.local>
2018-09-06 22:50:03 +0900

    list

--- app/controllers/kenki_controller.rb
+++ app/controllers/kenki_controller.rb
@@ -2,7 +2,11 @@ class KenkiController < ApplicationController
   def index
       @kenkihous= KenkiHou.all
       @likenkihous=LiKenkihou.all
+      @likenkihou1s=LiKenkihou.where(id:1..38)
+      @likenkihou2s=LiKenkihou.where(id:40..66)
+
   end
+
 
 
 end
