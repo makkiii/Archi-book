0844558a2de455909d22c56b15fdcca78f196ff7
shinomaki <makki@shinoharamakikonoMacBook.local>
2018-09-07 13:17:25 +0900

    linkjikken

--- app/controllers/kenkigraf_controller.rb
+++ app/controllers/kenkigraf_controller.rb
@@ -1,6 +1,8 @@
 class KenkigrafController < ApplicationController
   def graf
 
-     @kenkihous=KenkiHou.where(id:1705..1712)
+     @kenkihousgraf1=KenkiHou.where(id:1705..1712)
+     @kenkihousgraf2=KenkiHou.where(kijyun:'kijyun1712'..'kijyun1863')
+     @kenkihousgraf3=KenkiHou.where(kijyun:'kijyun1864'..'kijyun1905')
   end
 end
